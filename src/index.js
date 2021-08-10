977aee29119904173835e83ca9dad0fa9914f5d1
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2021-08-10 14:16:03 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/login", (req, res) => {
+    const code = req.body.code;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+    });
+
+    spotifyApi
+        .authorizationCodeGrant(code)
+        .then((data) => {
+            res.json({
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                expiresIn: data.body.expires_in,
+            });
+        })
+        .catch(() => res.sendStatus(400));
+});
+
+// Refreshes the access token every one hour
+app.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken,
+    });
+
+    spotifyApi
+        .refreshAccessToken()
+        .then((data) => {
+            res.json({
+                accessToken: data.body.access_token,
+                expiresIn: data.body.expires_in,
+            });
+        })
+        .catch(() => {
+            res.sendStatus(400);
+        });
+});
+
+app.listen(3333);
